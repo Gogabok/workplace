2a26207534af6f94021ac2f1800abaa616aa1fc6
Gogabok <gogabok123@gmail.com>
2019-11-09 17:57:58 +0300

    .

--- spin/main.js
+++ spin/main.js
@@ -1,20 +1,403 @@
-let nums = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 
-]
-
-let nums = [
+const nums = [
   {
-    
+    i: 0,
+    color: "gold",
+    colorHex: "#ffd434"
   },
-  
+  {
+    i: 1,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 2,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 3,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 4,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 5,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 6,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 7,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 8,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 9,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 10,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 11,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 12,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 13,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 14,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 15,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 16,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 17,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 18,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 19,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 20,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 21,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 22,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 23,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 24,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 25,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 26,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 27,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 28,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 29,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 30,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 31,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 32,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 33,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 34,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 35,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 36,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 37,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 38,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 39,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 40,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 41,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 42,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 43,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 44,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 45,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 46,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 47,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 48,
+    color: "gold",
+    colorHex: "#ffd434"
+  },
+  {
+    i: 49,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 50,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 51,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 52,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 53,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 54,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 55,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 56,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 57,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 58,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 59,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 60,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 61,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 62,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 63,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 64,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 65,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 66,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 67,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 68,
+    color: "green",
+    colorHex: "#39DC36"
+  },
+  {
+    i: 69,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
+  {
+    i: 70,
+    color: "blue",
+    colorHex: "#319CE9"
+  },
+  {
+    i: 71,
+    color: "red",
+    colorHex: "#ED3F22"
+  },
 ]
 
-let stepDeg = 360 / nums.length
+const animationDuration = 7000
+const timingBetweenRotating = 20000
+const stepDeg = 360 / nums.length
 let transformation = 0
 
 setInterval(() => {
   let randElement = nums[Math.floor(Math.random() * (nums.length))];
-  let transform = 720 + ( randElement * stepDeg )
-  transformation += transform
-  $('.colored-circle').css('transform', 'rotate(' + transformation + 'deg)');
-}, 8000);
+  let transform = 360 - (randElement.i * stepDeg)
+  transformation = transform
+  let currentRotate = transformation + 720
+  rotating(currentRotate)
+  setTimeout(() => {
+    fixing(randElement, transformation)
+  }, animationDuration);
+  console.log(transformation, randElement, currentRotate)
+}, timingBetweenRotating);
+
+
+function rotating(currentRotate) {
+  $('.colored-circle').css("transition-duration", + 7000 + "ms")
+  $('.colored-circle').css('transform', 'rotate(' + currentRotate + 'deg)');
+}
+
+function fixing(randElement, transformation) {
+  $('.colored-circle').css("transition-duration", "0s")
+  $('.colored-circle').css('transform', 'rotate(' + transformation + 'deg)')
+  $("<style>.x50_circle__arrow::after { border-color: " + randElement.colorHex + ' rgba(255, 212, 52, 0) rgba(255, 212, 52, 0)' + "; !important }</style>").appendTo("head")
+}
+
+
+
+
+let subOdometr = $(".x50_circle__counter");
+var odometr = new Odometer({
+  el: subOdometr,
+  minIntegerLen: 2,
+  format: 'dd',
+  //animation: 'count'
+
+});
