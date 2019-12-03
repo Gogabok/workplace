8894cd918facd74e32a0dd032158643eb5a783ab
Gogabok <gogabok123@gmail.com>
2019-12-03 23:58:13 +0300

    .

---
+++ bitlex folder/BITLEX GAME/ru/assets/bower_components/index.js
@@ -0,0 +1,96 @@
+"use strict";
+
+var $slides = undefined,
+    interval = undefined,
+    $selectors = undefined,
+    $btns = undefined,
+    currentIndex = undefined,
+    nextIndex = undefined;
+
+var cycle = function cycle(index) {
+	var $currentSlide = undefined,
+	    $nextSlide = undefined,
+	    $currentSelector = undefined,
+	    $nextSelector = undefined;
+
+	nextIndex = index !== undefined ? index : nextIndex;
+
+	$currentSlide = $($slides.get(currentIndex));
+	$currentSelector = $($selectors.get(currentIndex));
+
+	$nextSlide = $($slides.get(nextIndex));
+	$nextSelector = $($selectors.get(nextIndex));
+
+	$currentSlide.removeClass("active").css("z-index", "0");
+
+	$nextSlide.addClass("active").css("z-index", "1");
+
+	$currentSelector.removeClass("current");
+	$nextSelector.addClass("current");
+
+	currentIndex = index !== undefined ? nextIndex : currentIndex < $slides.length - 1 ? currentIndex + 1 : 0;
+
+	nextIndex = currentIndex + 1 < $slides.length ? currentIndex + 1 : 0;
+};
+
+$(function () {
+	currentIndex = 0;
+	nextIndex = 1;
+
+	$slides = $(".slide");
+	$selectors = $(".selector");
+	$btns = $(".btn");
+
+	$slides.first().addClass("active");
+	$selectors.first().addClass("current");
+
+	interval = window.setInterval(cycle, 6000);
+
+	$selectors.on("click", function (e) {
+		var target = $selectors.index(e.target);
+		if (target !== currentIndex) {
+			window.clearInterval(interval);
+			cycle(target);
+			interval = window.setInterval(cycle, 6000);
+		}
+	});
+
+	$btns.on("click", function (e) {
+		window.clearInterval(interval);
+		if ($(e.target).hasClass("prev")) {
+			var target = currentIndex > 0 ? currentIndex - 1 : $slides.length - 1;
+			cycle(target);
+		} else if ($(e.target).hasClass("next")) {
+			cycle();
+		}
+		interval = window.setInterval(cycle, 6000);
+	});
+});
+
+
+$(function(){
+  $(document).keypress(function(e) {
+    cwrite(e.which,'Keypress event');
+    if(e.which==120) custom_dialog_toggle('Keypress x','You opened this window by pressing x');
+  });
+});
+function custom_dialog_toggle(title,text,buttons) {
+  if(typeof title !=='undefined') $('#dlg-header').html(title);
+  if(typeof text !=='undefined') $('#dlg-content').html(text);
+  cwrite('Current state: '+$('#dialog_state').prop("checked"),'custom_dialog_toggle');
+  $('#dialog_state').prop("checked", !$('#dialog_state').prop("checked"));
+}
+// Console logging function for debugging
+// cwrite(str, title)
+//      str:              string to be appended to console
+//      title (optional): title of the string
+// (c)  Tuomas Hatakka 2015
+//      http://tuomashatakka.fi
+function cwrite(str,title) {
+  var ce = $('#console');
+  var sg = "<p>";
+  if(typeof title !=='undefined') sg = sg+"<em>"+title+"</em> ";
+  sg = sg+str+"</p>";
+  ce.prepend(sg);
+  //if(ce.children("p").length>6) ce.children("p").first().remove();
+}
