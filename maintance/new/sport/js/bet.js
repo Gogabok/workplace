55b3b89ecb44316ce00838178eb51f9f38977505
Gogabok <gogabok123@gmail.com>
2019-12-07 15:07:10 +0300

    .

---
+++ maintance/new/sport/js/bet.js
@@ -0,0 +1,187 @@
+window.addEventListener("load", function(){
+	document.getElementById("btn-bet").addEventListener("click", function(e){
+		const sum = document.getElementById('sum');
+		const prize = document.getElementById('prize');
+		const win = sum.value*50;
+        prize.innerHTML = win;
+   });
+
+    document.getElementById("f1").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f3").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f4").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f5").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f6").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f7").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f8").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f9").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f10").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f11").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f12").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f13").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+   });
+
+    document.getElementById("f14").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t1").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t2").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t3").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t4").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t5").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t6").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t7").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t8").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t9").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t10").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t11").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t12").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t13").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+
+     });
+
+    document.getElementById("t14").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+     });
+
+    document.getElementById("d1").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+     });
+
+    document.getElementById("d2").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+     });
+
+    document.getElementById("d3").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+     });
+
+    document.getElementById("w1").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+     });
+
+    document.getElementById("w2").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+     });
+
+    document.getElementById("d4").addEventListener("click", function(e){
+        this.innerHTML;
+        p2.innerHTML = this.innerHTML;
+     });
+});
