30b65458ba6996f3e6da044f153b2851346db312
Gogabok <51056095+Gogabok@users.noreply.github.com>
2020-03-20 16:14:51 +0300

    init

---
+++ punicapp/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+      <header class="header">
+        <img src="images/header.jpg" alt="">
+      </header>
+    <router-view/>
+  </div>
+</template>
+
---
+++ punicapp/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <price-calculate></price-calculate>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import PriceCalculate from "../components/pages/PriceCalculate";
+
+export default {
+  name: 'Home',
+  components: {
+    PriceCalculate,
+    HelloWorld
+  }
+}
+</script>
