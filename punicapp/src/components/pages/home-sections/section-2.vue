c639c1a1a046b895a0398e2367cbec07b78a3851
Gogabok <gogabok123@gmail.com>
2020-03-24 15:34:34 +0300

    .

--- punicapp/src/components/pages/home-sections/section-2.vue
+++ punicapp/src/components/pages/home-sections/section-2.vue
@@ -1,14 +1,28 @@
 <template>
   <section class="section section-2">
-   <p class="title">Наши услуги</p>
-   <p class="subtitle">
-     Разработка мобильных приложений и веб-проектов для бизнеса и стартапов
-   </p>
+    <div class="container">
+      <p class="title">Наши услуги</p>
+      <p class="subtitle">Разработка мобильных приложений и веб-проектов для бизнеса и стартапов</p>
+      <div class="icons">
+        <div :class="'icon-' + icon" class="icon" v-for="icon of 25" :key="icon">
+          <img
+            ondragstart="return false"
+            :src="require(`../../../assets/images/section-2/icon-${icon}.svg`)"
+            alt
+          />
+        </div>
+      </div>
+      <div class="cards-services">
+        <div class="cards-services-item">
+          
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Section-2'
-}
+  name: "Section-2"
+};
 </script>
