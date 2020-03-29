95e8d6de69571ef894cada37a17aa7f29dc17def
Gogabok <gogabok123@gmail.com>
2020-03-29 14:27:01 +0300

    .

---
+++ onlineChairsShop/assets/js/script.js
@@ -0,0 +1,18 @@
+
+let filters = document.getElementById("filters");
+
+document.getElementById("filters-switchers").addEventListener('click', () => {
+  if (filters.style.visibility === 'hidden') {
+    filters.style.visibility = 'visible';
+    filters.style.opacity = '1';
+  } else {
+    filters.style.visibility = 'hidden';
+    filters.style.opacity = '0';
+  }
+})
+
+document.getElementById("filters-success").addEventListener('click', () => {
+  filters.style.visibility = 'hidden';
+  filters.style.opacity = '0';
+})
+
